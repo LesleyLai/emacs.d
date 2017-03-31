998213013dbb8225ed7f40eafdfaaae3cce63ca8
Lesley Lai <laisililai@live.cn>
2017-03-31 13:55:14 -0600

    Ruby. Remove enhanced ruby mode, and some other functionalities.

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -1,28 +1,47 @@
-;; Enhanced ruby mode
+;; Rbenv for ruby version manage
 ;; {{
-(require-package 'enh-ruby-mode)
-(remove-hook 'enh-ruby-mode-hook 'erm-define-faces)
-
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
-
-(setq enh-ruby-bounce-deep-indent t)
-(setq enh-ruby-hanging-brace-indent-level 2)
-
-
-(setq enh-ruby-bounce-deep-indent t)
-(setq enh-ruby-hanging-brace-indent-level 2)
+(require-package 'rbenv)
+(global-rbenv-mode)
+(rbenv-use-global)
 ;; }}
+
+;; Seeing is believing for Ruby buffer interaction {{
+(require-package 'seeing-is-believing)
+(setq seeing-is-believing-prefix "C-.")
+(add-hook 'ruby-mode-hook 'seeing-is-believing)
+(require 'seeing-is-believing)
+;; }}
+
+;; Ruby inferior REPL {{
+(require-package 'inf-ruby)
+(autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
+(add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
+;; }}
+
+;; Test current file directly
+;; {{
+(require-package 'ruby-test-mode)
+(require 'ruby-test-mode)
+(add-hook 'ruby-mode-hook 'ruby-test-mode)
+
+;; http://worace.works/2016/06/07/getting-started-with-emacs-for-ruby/
+(add-hook 'compilation-finish-functions
+          (lambda (buf strg)
+            (switch-to-buffer-other-window "*compilation*")
+            (read-only-mode)
+            (goto-char (point-max))
+            (local-set-key (kbd "q")
+                           (lambda () (interactive) (quit-restore-window)))))
+;; }}
+
+;; auto-matching
+(require-package 'ruby-electric)
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
 
 ;; Robe mode
 ;; {{
 (require-package 'robe)
-(add-hook 'enh-ruby-mode-hook 'robe-mode)
+(add-hook 'ruby-mode-hook 'robe-mode)
 (eval-after-load 'company
   '(push 'company-robe company-backends))
 ;; }}
