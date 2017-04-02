779ad234ae3a48b825bc09704b32b6541dd0bd7e
Lesley Lai <laisililai@live.cn>
2017-04-02 13:34:51 -0600

    Recompile .el file on save

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -1,13 +1,14 @@
 ;; Emacs lisp specify setup
 
-(defun remove-elc-on-save ()
+(defun recompile-elc-on-save ()
   "If you're saving an elisp file, likely the .elc is no longer valid."
   (make-local-variable 'after-save-hook)
   (add-hook 'after-save-hook
             (lambda ()
-              (if (file-exists-p (concat buffer-file-name "c"))
-                  (delete-file (concat buffer-file-name "c"))))))
-(add-hook 'emacs-lisp-mode-hook 'remove-elc-on-save)
+              (if (file-exists-p (byte-compile-dest-file buffer-file-name))
+                  (byte-compile-file buffer-file-name)))))
+(add-hook 'emacs-lisp-mode-hook 'recompile-elc-on-save
+          )
 
 
 (defun elisp-mode-hook-setup ()
