2ccfec09e49788e32a2c35febf1ec83d60f31863
Lesley Lai <laisililai@live.cn>
2017-04-02 17:03:49 -0600

    Glsl mode

---
+++ lisp/init-glsl-mode.el
@@ -0,0 +1,9 @@
+(use-package glsl-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode)))
+
+(provide 'init-glsl-mode)
