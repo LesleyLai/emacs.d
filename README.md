c149898968834dd8bd7e762e3f67ca9fb46366f3
Lesley Lai <laisililai@live.cn>
2017-04-16 10:52:53 -0600

    Web completion/ A little readme

--- README.md
+++
@@ -1,5 +0,0 @@
-# emacs.d
-My own setting of emacs
-
-## Usage
-Put the whole directory to **~/.emacs.d**
--- lisp/init-web.el
+++ lisp/init-web.el
@@ -2,8 +2,8 @@
 
 (use-package web-mode
   :ensure t
-  :after company company-web-html
-  :config
+  :after company company-web-html company-tern
+  :init
   (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
@@ -16,12 +16,29 @@
   (add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.rhtml\\(\\.erb\\)?\\'" . web-mode)) ; ruby
   (add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'"  . web-mode)) ; ruby
-
-  
+  :config  
   (add-hook 'web-mode-hook (lambda ()
                              (set (make-local-variable 'company-backends)
-                                  '(company-web-html))
+                                  '(company-tern
+                                    company-web-html
+                                    company-css
+                                    company-yasnippet
+                                    company-files))
                              (company-mode t)))
+
+  ;; Enable JavaScript completion between <script>...</script> etc.
+  (defadvice company-tern (before web-mode-set-up-ac-sources activate)
+    "Set `tern-mode' based on current language before running company-tern."
+    (message "advice")
+    (if (equal major-mode 'web-mode)
+        (let ((web-mode-cur-language
+               (web-mode-language-at-pos)))
+          (if (or (string= web-mode-cur-language "javascript")
+                  (string= web-mode-cur-language "jsx")
+                  )
+              (unless tern-mode (tern-mode))
+            (if tern-mode (tern-mode -1))))))
+
 
   (eval-after-load 'web-mode
     '(progn
