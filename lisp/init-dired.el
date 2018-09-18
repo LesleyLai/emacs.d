c1bcf08d00ba93b2cf1faa95c56dac54030f60c2
Lesley <lesleylai@lesleylai.info>
2018-09-17 18:25:10 -0600

    dired-sidebar

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -7,4 +7,9 @@
 ;; auto refresh dired when file changes
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure nil
+  :commands (dired-sidebar-toggle-sidebar))
+
 (provide 'init-dired)
