af8359d8a91bd95ea1c11889aeaf76a388af1241
Lesley <lesleylai@lesleylai.info>
2018-10-15 14:40:41 -0600

    Use xah-fly-keys back

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,11 +2,8 @@
 ;; ui for git
 ;; ============================================
 (use-package magit
-  :after modalka
   :ensure t
   :bind ("C-x g" . magit-status)
-  :config
-  (define-key modalka-mode-map (kbd "SPC g") #'magit-status)
   )
 
 (provide 'init-git)
