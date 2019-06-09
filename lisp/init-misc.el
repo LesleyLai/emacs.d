9ac5dc008b1f5667f7a01315a248b4fcfd0aa5a5
Lesley Lai <laisililai@live.cn>
2019-06-08 23:06:47 -0600

    Benchmark initialization

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -87,4 +87,11 @@
 
 (customize-set-variable 'recentf-auto-cleanup 'never)
 
+;; benchmark initialization
+(use-package benchmark-init
+  :ensure t
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 (provide 'init-misc)
