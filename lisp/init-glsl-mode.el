aa1c193c770ec9f09e07bafd64e6249952143ec0
Lesley Lai <lesley@sketchup.com>
2019-08-14 11:17:35 -0600

    more file extensions for glsl mode

--- lisp/init-glsl-mode.el
+++ lisp/init-glsl-mode.el
@@ -4,6 +4,10 @@
   (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
   (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
   (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode)))
+  (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.comp\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.tesc\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.tese\\'" . glsl-mode))
+  )
 
 (provide 'init-glsl-mode)
