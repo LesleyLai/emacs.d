9b8a0c0d5048212d3238174f6832392c8003f47f
Lesley <lesleylai@lesleylai.info>
2020-01-22 01:42:06 -0700

    Disable spell checking for now

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -1,14 +1,12 @@
 ;;; init-spelling.el --- Spell checking
 
-(if (not *win64*)
-    (use-package flyspell-mode
-      :defer t
-      :config
-      (progn
-        (add-hook 'text-mode-hook (lambda () (flyspell-mode 1)))
-        (customize-set-variable 'ispell-program-name "aspell")
-        (customize-set-variable 'ispell-list-command "--list") ;; run flyspell with aspell, not ispell
-        )
-      ))
+;; (use-package flyspell-mode              ;
+;;   :defer t
+;;   :config
+;;   (progn
+;;     (add-hook 'text-mode-hook (lambda () (flyspell-mode 1)))
+;;     (customize-set-variable 'ispell-program-name "hunspell")
+;;     )
+;;   )
 
 (provide 'init-spelling)
