ac6c0cd1dbb11c01601307f89d149736cf177f0c
Lesley Lai <lesley@lesleylai.info>
2020-11-29 08:26:17 -0700

    Only reduce magit ui on Windows

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -6,12 +6,13 @@
   :defer t
   :bind ("C-x g" . magit-status)
   :config
-  (remove-hook 'magit-status-sections-hook 'magit-insert-tags-header)
-  (remove-hook 'magit-status-sections-hook 'magit-insert-status-headers)
-  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-pushremote)
-  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-pushremote)
-  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-upstream)
-  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent))
+  (if *win64*
+      (progn (remove-hook 'magit-status-sections-hook 'magit-insert-tags-header)
+             (remove-hook 'magit-status-sections-hook 'magit-insert-status-headers)
+             (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-pushremote)
+             (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-pushremote)
+             (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-upstream)
+             (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent))))
 
 (use-package gitattributes-mode :ensure t)
 (use-package gitconfig-mode :ensure t)
