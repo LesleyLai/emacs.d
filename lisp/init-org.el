e9cd634d875eacf8c296b7fc44900151d2bfabd5
Lesley Lai <lesley@lesleylai.info>
2020-12-27 14:38:41 -0700

    Automatically done a org-mode task when all checkbox clicked

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -81,6 +81,33 @@
      "Babel"
      (("'" org-edit-src-code "Edit source"))))
 
+  ;; https://emacs.stackexchange.com/questions/19843/how-to-automatically-adjust-an-org-task-state-with-its-children-checkboxes
+  (defun my/org-checkbox-todo ()
+    "Switch header TODO state to DONE when all checkboxes are ticked, to TODO otherwise"
+    (let ((todo-state (org-get-todo-state)) beg end)
+      (unless (not todo-state)
+        (save-excursion
+          (org-back-to-heading t)
+          (setq beg (point))
+          (end-of-line)
+          (setq end (point))
+          (goto-char beg)
+          (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+                                 end t)
+              (if (match-end 1)
+                  (if (equal (match-string 1) "100%")
+                      (unless (string-equal todo-state "DONE")
+                        (org-todo 'done))
+                    (unless (string-equal todo-state "TODO")
+                      (org-todo 'todo)))
+                (if (and (> (match-end 2) (match-beginning 2))
+                         (equal (match-string 2) (match-string 3)))
+                    (unless (string-equal todo-state "DONE")
+                      (org-todo 'done))
+                  (unless (string-equal todo-state "TODO")
+                    (org-todo 'todo)))))))))
+  (add-hook 'org-checkbox-statistics-hook 'my/org-checkbox-todo)
+
   :custom
   (org-src-tab-acts-natively t)
   (org-agenda-span 'day)
